36c36e6705a95df203733d74b2322efd010b2672
Ariel Ross <arielross92@gmail.com>
2018-01-06 01:13:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 function theBeatlesPlay (musicians, instruments){
   var empty = []
-  musicians = musicians
-  instruments = instruments
   for (var i=0; i<musicians.length; i++){
     var string = musicans[i] + " plays " + instruments[i]
     empty.push(string)
